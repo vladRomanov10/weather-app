c7cd7810ec0be9a10d10fd5a46543186203a4783
Vlad Romanov <vladromeur@gmail.com>
2024-04-29 17:27:46 +0500

    Финальная версия модуля с часами

--- js/clock.js
+++ js/clock.js
@@ -5,7 +5,7 @@ const getCurrentTime = () => {
     const data = new Date()
     let hour = data.getHours().toString()
     let minutes = data.getMinutes().toString()
-    let seconds = data.getSeconds()
+    let seconds = data.getSeconds().toString()
     
     if (hour.length < 2) {
         hour = '0' + hour
@@ -15,24 +15,21 @@ const getCurrentTime = () => {
         minutes = '0' + minutes
     }
 
+    if(seconds.length < 2) {
+        seconds = '0' + seconds
+    }
     document.getElementById('currentTime').innerHTML = `${hour}:${minutes}:${seconds}`
 }
 
 getCurrentTime()
 //Отрисовываем текущее время каждую секунду
 let setIntervalId = setInterval(getCurrentTime, 1000)
-//Проверяем активна ли вкладка в браузере, если нет, отключаем время
-//если пользователь вернулся на вкладку, запускаем время
-//отключение отсчета времени работает, только при первом переходе из вкладки
-//при втором переходе, время продолжает идти, хотя вкладка неактивна
-const isPageVisibility = () => {
-    document.addEventListener('visibilitychange', () => {
-        if (document.hidden) {
-            clearInterval(setIntervalId)
-        } else {
-            setIntervalId = setInterval(getCurrentTime, 1000)
-        }
-    })
-}
+//Останавливаем время, когда пользователь уходит из вкладки и возобновляем, когда приходит
+document.addEventListener('visibilitychange', () => {
+    if (document.hidden) {
+        clearInterval(setIntervalId)
+    } else {
+        setIntervalId = setInterval(getCurrentTime, 1000)
+    }
+})
 
-setInterval(isPageVisibility, 1000)
