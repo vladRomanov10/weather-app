345fbae74e16937a842c3d69e36e815d5ea84b5e
Vlad Romanov <vladromeur@gmail.com>
2024-05-30 12:24:57 +0500

    Работа над реквестом

--- js/tabs.js
+++ js/tabs.js
@@ -1,40 +1,43 @@
 const tabs = document.querySelectorAll('.tabs__item')
     
 //Функция, удаляющая класс .widget_hide
-const classHideRemove = () => document.querySelectorAll('.widget_hide').forEach(el => {
+const removeClassHide = () => document.querySelectorAll('.widget_hide').forEach(el => {
     el.classList.remove('widget_hide')
 })
 
 //Функция, добовляющая класс widget_hide для элементов, которые необходимо скрыть
-const classHideAdd = (class1, class2) => document.querySelectorAll(`${class1}, ${class2}`).forEach(el => {
+const addClassHide = (class1, class2) => document.querySelectorAll(`${class1}, ${class2}`).forEach(el => {
     el.classList.add('widget_hide')
 })
+
+//Функция, удаляющая класс .tabs_active 
+const removeClassActive = () => document.querySelector('.tabs_active').classList.remove('tabs_active')
+
+//Функция, добовляющая класс .tabs_active на таб по которому кликнули
+const addClassActive = (event) => event.target.classList.add('tabs_active')
 
 //Обработчик клика на таб
 const tabClickHandler = (event) => {
     
-    //Находим активный таб и убираем класс активная активный таб
-    document.querySelector('.tabs_active').classList.remove('tabs_active')
-
-    //Добавляем класс активный таб на таб по которому кликнули
-    event.target.classList.add('tabs_active')
+    removeClassActive()
+    addClassActive(event)
 
     //отображение контента соответсвующего кликнутому табу
     switch (event.target.id) {
 
         case 'tabNow':
-            classHideRemove()
-            classHideAdd('.tab-details', '.tab-forecast')
+            removeClassHide()
+            addClassHide('.tab-details', '.tab-forecast')
             break
 
         case 'tabDetails':
-            classHideRemove()
-            classHideAdd('.tab-now', '.tab-forecast')
+            removeClassHide()
+            addClassHide('.tab-now', '.tab-forecast')
             break
 
         case 'tabForecast':
-            classHideRemove()
-            classHideAdd('.tab-now', '.tab-details')
+            removeClassHide()
+            addClassHide('.tab-now', '.tab-details')
             break
     }
 }
