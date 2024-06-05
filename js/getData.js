853c807d3b8655315dbe7424c0b7a420e15d3dcb
Vlad Romanov <vladromeur@gmail.com>
2024-06-05 16:16:51 +0500

    Переделал виджеты из tab-forcast

--- js/getData.js
+++ js/getData.js
@@ -1,8 +1,5 @@
 //а.ф. для запроса погоды на сервер
 const getData = async(url) => {
     const res = await fetch(url)
-    const data = await res.json()
-    //для удобстава разработки вывожу объект с погодой в консоль
-    console.log(data)
-    return data
+    return await res.json()
 }
